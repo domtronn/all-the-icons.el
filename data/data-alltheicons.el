34c03cf8f64850046fb902da8303f5089b24ae49
Dom Charlesworth <dgc336@gmail.com>
2016-06-12 11:06:31 +0100

    Add the generated data maps for each font

---
+++ data/data-alltheicons.el
@@ -0,0 +1,26 @@
+(defvar alltheicons-alist
+  '(
+
+    ( "jasmine"        . "\xe901" )
+    ( "grunt"          . "\xe90c" )
+    ( "d3"             . "\xe90d" )
+    ( "c-line"         . "\xe90e" )
+    ( "c"              . "\xe914" )
+    ( "cplusplus"      . "\xe912" )
+    ( "cplusplus-line" . "\xe90f" )
+    ( "csharp"         . "\xe910" )
+    ( "csharp-line"    . "\xe911" )
+    ( "coffeescript"   . "\xe913" )
+    ( "scala"          . "\xe907" )
+    ( "apache"         . "\xe908" )
+    ( "script"         . "\xe909" )
+    ( "less"           . "\xe90a" )
+    ( "aws"            . "\xe90b" )
+    ( "svg"            . "\xe900" )
+    ( "perl"           . "\xe904" )
+    ( "javascript"     . "\xe905" )
+    ( "git"            . "\xe906" )
+
+    ))
+
+(provide 'data-alltheicons)
