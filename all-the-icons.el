e5c9040d73de8ee6ccee8a9be99936c6e1084f6a
Dom Charlesworth <dgc336@gmail.com>
2016-07-30 13:30:23 +0100

    Shell quote argument when checking for submodules

--- all-the-icons.el
+++ all-the-icons.el
@@ -380,9 +380,11 @@
   (when (and (file-exists-p (format "%s/.git" dir))
              (locate-dominating-file dir ".gitmodules"))
 
-    (let* ((modules-file (format "%s.gitmodules" (locate-dominating-file dir ".gitmodules")))
+    (let* ((modules-file (expand-file-name (format "%s.gitmodules" (locate-dominating-file dir ".gitmodules"))))
            (module-file (file-name-base dir))
-           (cmd (format "cat %s | grep submodule | awk '{print $2}' | grep %s" modules-file module-file)))
+           (cmd (shell-quote-string
+                 (format "cat %s | grep submodule | awk '{print $2}' | grep %s"
+                         (shell-quote-argument modules-file) module-file))))
 
       (not (eq "" (shell-command-to-string cmd))))))
 
