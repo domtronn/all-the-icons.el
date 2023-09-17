faf4d5c8db30e0e1be22b8a9b560a4b4d0184a39
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2023-09-17 17:50:46 +0100

    search inherited faces too

--- all-the-icons.el
+++ all-the-icons.el
@@ -1188,8 +1188,8 @@ PADDING is the number of pixels to be applied to the SVG image."
                (setf (image-property icon :ascent) 'center)
                (setf (image-property icon :margin) (or custom-padding ,padding))
                (when face
-                 (setf (image-property icon :foreground) (face-foreground face))
-                 (setf (image-property icon :background) (face-background face)))
+                 (setf (image-property icon :foreground) (face-foreground face nil t))
+                 (setf (image-property icon :background) (face-background face nil t)))
 
                (propertize "￼"
                            'face (or face 'default)           ;so that this works without `font-lock-mode' enabled
